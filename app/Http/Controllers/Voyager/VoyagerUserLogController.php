58f1b43c00861d5b6ef34dc63e1e58f5a92f5765
Hamza <hamzabitup@gmail.com>
2019-07-08 14:38:03 +0100

    nearly done with the userlog

---
+++ app/Http/Controllers/Voyager/VoyagerUserLogController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Voyager;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Contracts\Activity;
+use Spatie\Activitylog\Models\Activity as ActivityModel;
+use TCG\Voyager\Http\Controllers\VoyagerUserController as BaseVoyagerUserController;
+
+class VoyagerUserLogController extends BaseVoyagerUserController
+{
+    /**
+     * Get the user log
+     * 
+     * @param id integer
+     * @return response
+     */
+    public function indexLog($id) {
+        $userLog = ActivityModel::where('subject_id', '=', $id)->orderBy('created_at', 'desc')->paginate(2);
+
+        return response()->json([
+            'userLog' => $userLog,
+            'numberOfPages' => $userLog->lastPage(),
+            'currentPageNumber' => $userLog->currentPage(),
+            'hasMorePages' => $userLog->hasMorePages()
+        ]);
+    }
+}
