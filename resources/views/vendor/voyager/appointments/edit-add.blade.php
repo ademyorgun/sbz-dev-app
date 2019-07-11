1037f7378002a18d75133e00b9cbacf751ba1116
Hamza <hamzabitup@gmail.com>
2019-07-11 18:15:03 +0100

    view again

--- resources/views/vendor/voyager/appointments/edit-add.blade.php
+++ resources/views/vendor/voyager/appointments/edit-add.blade.php
@@ -85,7 +85,52 @@
                                     @elseif($row->type == 'relationship' && $row->field == 'appointment_belongsto_user_relationship')
                                         {{-- we'd like to show a preselected user --}}    
                                         {{-- @include('voyager.formFields.relationshipUser ', ['options' => $row->details]) --}}
-                                        <h1>needs to be fixed asap</h1>
+                                        @php
+                                            $options = $row->details;
+                                        @endphp
+                                        @if(isset($options->model) && isset($options->type))
+                                            @if(class_exists($options->model))
+
+                                                @php $relationshipField = $row->field; @endphp
+
+                                                @if($options->type == 'belongsTo')
+
+                                                    @if(isset($view) && ($view == 'browse' || $view == 'read'))
+
+                                                        @php
+                                                            $relationshipData = (isset($data)) ? $data : $dataTypeContent;
+                                                            $model = app($options->model);
+                                                            $query = $model::where($options->key,$relationshipData->{$options->column})->first();
+                                                        @endphp
+
+                                                        @if(isset($query))
+                                                            <p>{{ $query->{$options->label} }}</p>
+                                                        @else
+                                                            <p>{{ __('voyager::generic.no_results') }}</p>
+                                                        @endif
+
+                                                    @else
+                                                        
+                                                        <select
+                                                            class="form-control " name="{{ $options->column }}"
+                                                            data-get-items-field="{{$row->field}}"
+                                                        >
+                                                            <option value="{{ auth()->user()->id }}">{{ auth()->user()->user_name }}</option>
+
+                                                        </select>
+
+
+                                                    @endif
+
+                                                @endif
+
+                                            @else
+
+                                                cannot make relationship because {{ $options->model }} does not exist.
+
+                                            @endif
+
+                                        @endif
                                     @elseif ($row->type == 'relationship')
                                         @include('voyager::formfields.relationship', ['options' => $row->details])
                                     @else
