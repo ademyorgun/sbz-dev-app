e5e39b87cf9f848f00037d2c50811b78ce49f9b8
Hamza <hamzabitup@gmail.com>
2019-07-11 15:57:25 +0100

    new routes

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,14 @@ Auth::routes();
     
 // });
 
+
 Voyager::routes();
 
+// testing route for filtering the items
+Route::post('products/filter', 'Voyager\VoyagerProductsController@filter');
+
+// User log
+Route::get('users/{id}/log', 'Voyager\VoyagerUserLogController@indexLog')->name('user.log');
+
+// 
+Route::post('appointments/filter', 'Voyager\VoyagerAppointmentController@filter')->name('appointment.filter');
