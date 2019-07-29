ae5b6c4770cc51ae21bcf9ae855c322b95b8afa8
Hamza <hamzabitup@gmail.com>
2019-07-29 13:08:35 +0100

    fixed error

--- app/User.php
+++ app/User.php
@@ -50,6 +50,6 @@ class User extends \TCG\Voyager\Models\User
      * Get the appointments for the user
      */
     public function appointments() {
-        return $this->hasMany('App\Appointment', 'call_agent_id', 'id');
+        return $this->hasMany('App\Appointment', 'sales_agent_id', 'id');
     }
 }
