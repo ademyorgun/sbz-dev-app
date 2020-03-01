d22cc50e9ed57f036fce543a3bb50a28b32e63d6
Hamza <hamzabitup@gmail.com>
2020-03-01 12:13:20 +0100

    Test; remove crsf token

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            // \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
